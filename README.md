bd9418d9bd565465c9f47720b1e97215814d3dbc
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-06-15 13:45:58 +0200

    Returns configured winston logger instances without any transports

---
+++ README.md
@@ -0,0 +1,6 @@
+# example
+
+```
+const logging = require('mxd-silent-logging')(config);
+const appLogger = logging('app');
+```
---
+++ src/lib.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const winston = require('winston');
+
+module.exports = (config) => (category, options) => {
+  const logger = new winston.Logger(options);
+  return logger;
+};
